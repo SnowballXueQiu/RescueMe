1eca66e2cfa17d0380868f7cc2dcf6cab3391874
Snowball_233 <wangrb.qq@outlook.com>
2023-09-16 23:42:55 +0800

    fix(slug): Unresolved variable authors

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,30 +2,34 @@
     <div class="py-10 px-7">
         <content-doc v-slot="{doc}">
             <transition-group name="slide" @enter="onEnter">
-                <div class="prose mb-4 dark:prose-invert dark:prose-headings:text-gray-300" data-delay="1" v-if="finish">
+                <div v-if="finish" class="prose mb-4 dark:prose-invert dark:prose-headings:text-gray-300"
+                     data-delay="1">
                     <h1>{{ doc.title }}</h1>
                 </div>
-                <content-renderer :value="doc" class="
+                <content-renderer v-if="finish" key="renderer" :value="doc" class="
                         dark:prose-invert
                         prose-p:text-gray-700 dark:prose-p:text-gray-300
                         dark:prose-headings:text-gray-200
                         prose prose-img:border prose-img:border-solid prose-img:border-gray-200
                         dark:prose-img:border-gray-700
                         prose-img:rounded-md max-w-full mx-auto
-                    " key="renderer" v-if="finish" data-delay="2" />
-                <footer class="w-full mt-4 text-right opacity-50 text-gray-600 font-sans text-sm dark:text-gray-300" key="footer" v-if="finish">
-                    <a v-for="([author,email], idx) in doc.authors" :href="`mailto:${email}`" class="underline-offset-4 hover:underline">
-                        {{ author }} {{ idx === doc.authors.length-1 ? '' : ', ' }}
+                    " data-delay="2"/>
+                <footer v-if="finish"
+                        key="footer"
+                        class="w-full mt-4 text-right opacity-50 text-gray-600 font-sans text-sm dark:text-gray-300">
+                    <a v-for="([author,email], idx) in doc['authors']" :href="`mailto:${email}`"
+                       class="underline-offset-4 hover:underline">
+                        {{ author }} {{ idx === doc['authors'].length - 1 ? '' : ', ' }}
                     </a>
-                    <div v-if="doc.copyright" class="mt-1 w-fit mr-0 ml-auto">
+                    <div v-if="doc['copyright']" class="mt-1 w-fit mr-0 ml-auto">
                         <span class="underline-offset-4 hover:underline">
-                            &copy; <a :href="doc.copyright.url">{{ doc.copyright.name }}</a>
+                            &copy; <a :href="doc['copyright'].url">{{ doc['copyright'].name }}</a>
                         </span>
                     </div>
                 </footer>
             </transition-group>
         </content-doc>
-        
+
     </div>
 </template>
 
@@ -34,14 +38,14 @@
 
 <script lang="ts" setup>
 const finish = ref(false);
-setTimeout(()=>finish.value = true, 100)
+setTimeout(() => finish.value = true, 100)
 let depth = 20;
-const onEnter = (el:Element, done: ()=>void) => {
-    if (depth >= 0){
+const onEnter = (el: Element, done: () => void) => {
+    if (depth >= 0) {
         const delay = Number((el as HTMLElement).dataset.delay ?? 0) * 1000;
-        setTimeout(()=>{
+        setTimeout(() => {
             done();
-        },delay);
+        }, delay);
         depth -= 1;
     } else {
         done();
