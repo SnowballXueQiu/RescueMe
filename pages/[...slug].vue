b4f3a182031c8a7cfb07e367c2fcdead70a29cb6
Snowball_233 <wangrb.qq@outlook.com>
2023-09-18 22:13:03 +0800

    feat(slug): get GitHub stats (not finish)

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -57,7 +57,7 @@
                                         <path
                                             d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327 .668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path>
                                     </svg>
-                                    <span>3</span>
+                                    <span>{{ stargazersCount }}</span>
                                 </div>
                             </div>
                         </div>
@@ -86,4 +86,30 @@ const onEnter = (el: Element, done: () => void) => {
         done();
     }
 }
+</script>
+
+<script lang="ts">
+import axios from "axios";
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const stargazersCount = ref(0);
+
+        const apiUrl = 'https://api.github.com/repos/SnowballXueQiu/RescueMe';
+
+        axios.get(apiUrl)
+            .then(response => {
+                const jsonData = response.data;
+                stargazersCount.value = jsonData.stargazers_count;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+        return {
+            stargazersCount,
+        };
+    },
+};
 </script>
