b37e35000e5ce235830b76681fd082b9a844e9b5
GaoNeng-wWw <gaonenggg@outlook.com>
2023-09-23 21:43:17 +0800

    feat: menu

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <logo />
+        <content-navigation v-slot="{navigation}">
+            <ul class="mx-auto">
+            <template v-for="nav in navigation">
+                <nuxt-link :to="`/posts/${nav.title}`">
+                    <li
+                        v-if="!nav._draft"
+                        class="
+                        cursor-pointer transition-all duration-500 my-5
+                        text-lg xl:text-xl md:text-lg
+                        dark:text-gray-400 hover:dark:text-gray-100
+                        "
+                    >
+                        {{ nav.title }}
+                    </li>
+                </nuxt-link>
+            </template>
+        </ul>
+        </content-navigation>
+    </div>
+</template>
---
+++ pages/posts/[...slug].vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <p @click="back()" class="cursor-pointer dark:hover:text-gray-100 dark:text-gray-300 ">Back</p>
+        <transition-group :css="false" @enter="onEnter">
+            <content-renderer v-if="docFinish" key="renderer" :value="parsedContent" class="
+                    transition-all duration-1000
+                    dark:prose-invert
+                    prose-p:text-gray-700 dark:prose-p:text-gray-300
+                    dark:prose-headings:text-gray-200
+                    prose prose-img:border prose-img:border-solid prose-img:border-gray-200
+                    dark:prose-img:border-gray-700
+                    prose-img:rounded-md max-w-full mx-auto" data-delay="1.2"/>
+        </transition-group>
+        <section
+                v-if="docFinish"
+                key="footer"
+                class="transition-all w-full mt-4 text-right opacity-50 text-gray-600 font-sans text-sm dark:text-gray-100">
+            <a v-for="([author], idx) in parsedContent?.authors"
+               class="underline-offset-4">
+                {{ author }} {{ idx === parsedContent?.authors.length - 1 ? '' : ', ' }}
+            </a>
+            <br/>
+            <div v-if="parsedContent?.copyright" class="mt-1 w-fit mr-0 ml-auto">
+                <span class="underline-offset-4 hover:underline">
+                    &copy; <a :href="parsedContent?.copyright.url">{{ parsedContent?.copyright.name }}</a>
+                </span>
+            </div>
+        </section>
+    </div>
+</template>
+
+<style>
+</style>
+
+<script lang="ts" setup>
+import { ParsedContent } from '@nuxt/content/dist/runtime/types';
+const finish = ref(false);
+const docFinish = ref(false);
+const onEnter = (el: Element, done: () => void) => {
+    const delay = Number((el as HTMLElement).dataset.delay ?? 0) * 1000;
+    const element = el as HTMLElement;
+    element.style.opacity = '0';
+    element.style.transform = 'translateY(-20px)';
+    setTimeout(() => {
+        element.style.opacity = '1';
+        element.style.transform = 'translateY(0px)';
+        done();
+    }, delay);
+}
+const parsedContent = ref<ParsedContent>();
+const router = useRouter();
+const back = () => router.back();
+useAsyncData('content', () => queryContent('/').find())
+.then((content)=>{
+    if (content.data.value){
+        parsedContent.value=content.data.value[0];
+    }
+    docFinish.value = true;
+})
+</script>
